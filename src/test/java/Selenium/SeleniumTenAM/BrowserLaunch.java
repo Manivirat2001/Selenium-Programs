84c6e8bf664f5a0e09b80c524806963870454967
Manivirat2001 <137140965+Manivirat2001@users.noreply.github.com>
2023-06-22 10:37:42 +0530

    Update BrowserLaunch.java

--- src/test/java/Selenium/SeleniumTenAM/BrowserLaunch.java
+++ src/test/java/Selenium/SeleniumTenAM/BrowserLaunch.java
@@ -10,12 +10,9 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class BrowserLaunch {
 
 	public static void main(String[] args) {
-
 		WebDriverManager.chromedriver().setup();
-
 		ChromeDriver driver = new ChromeDriver();
 		driver.get("https://www.facebook.com/");
-
 		// WebDriverManager.edgedriver().setup();
 		// EdgeDriver driver = new EdgeDriver();
 
