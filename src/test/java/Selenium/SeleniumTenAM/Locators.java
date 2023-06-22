d89fff49866355ee22f4ecc2eb41ccd33143edd1
Manivirat2001 <137140965+Manivirat2001@users.noreply.github.com>
2023-06-22 10:38:53 +0530

    Update Locators.java

--- src/test/java/Selenium/SeleniumTenAM/Locators.java
+++ src/test/java/Selenium/SeleniumTenAM/Locators.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Locators {
 
 	public static void main(String[] args) {
-
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
